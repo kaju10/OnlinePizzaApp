8b6ae731d89529658850070d60e8ba175bfccc0b
aritr <aritr@LAPTOP-TV58P7I5>
2021-07-22 11:08:46 +0530

    first commit

---
+++ online_pizza_app/src/test/java/com/online_pizza_app/OnlinePizzaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online_pizza_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePizzaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
